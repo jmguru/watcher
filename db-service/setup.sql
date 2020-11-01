cc5c80b2920bba3ab11ac12e9ffee6e92bf41b81
jmguru <jmguru@gmail.com>
2020-11-01 11:53:48 -0600

    Use raspi sanctioned node

--- db-service/setup.sql
+++ db-service/setup.sql
@@ -6,11 +6,3 @@ CREATE TABLE `fooddata` (
   `food` varchar(80) NOT NULL DEFAULT '',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
-
-INSERT INTO `fooddata` (`id`, `fatty`, `dt`, `food`)
-VALUES
-        (1,'john',now(),'Banana'),
-        (2,'john',now(),'Turtle meat'),
-        (3,'terri',now(),'Turkey sandwich'),
-        (1,'terri',now(),'cookies'),
-        (1,'john',now(),'Poop on crackers');
