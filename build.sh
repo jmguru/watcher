f072e5ac34520984c0d97623c98375ce6064e1f5
John Griffith <griffitj@us.ibm.com>
2022-02-14 11:27:23 -0800

    default to linux64

--- build.sh
+++ build.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 HERE=$(pwd)
 
-arch=$(dpkg --print-architecture)
 export NODE_VERSION=node:8
 export DB_VERSION=mariadb:latest
 
-if [[ $arch == "arm"* ]]; then
-  export NODE_VERSION=hypriot/rpi-node:8
-  export DB_VERSION=hypriot/rpi-mysql
-fi
+### Rpi
+#export NODE_VERSION=hypriot/rpi-node:8
+#export DB_VERSION=hypriot/rpi-mysql
 
 rm -f $HERE/db-service/Dockerfile
 sed "s/_DBVERSION_/${DB_VERSION}/" $HERE/db-service/Dockerfile.template > $HERE/db-service/Dockerfile
@@ -16,4 +14,4 @@ sed "s/_DBVERSION_/${DB_VERSION}/" $HERE/db-service/Dockerfile.template > $HERE/
 rm -f $HERE/food-service/Dockerfile
 sed "s/_NODEVERSION_/$NODE_VERSION/" $HERE/food-service/Dockerfile.template > $HERE/food-service/Dockerfile
 
-docker-compose up --build
+docker-compose up -d --build
